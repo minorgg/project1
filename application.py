1a974a9315aca7f918f21501ae6b224dd780d1ed
minorgg <42815276+minorgg@users.noreply.github.com>
2019-03-08 16:02:31 +0100

    navbar

--- application.py
+++ application.py
@@ -18,5 +18,16 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     return render_template("index.html")
 
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
