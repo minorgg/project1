f531983cdf61d611829ca10c9891a7715199867d
minorgg <42815276+minorgg@users.noreply.github.com>
2019-03-08 14:32:57 +0100

    new upload

--- application.py
+++ application.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, session, render_template
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+# Set up database
+engine = create_engine("postgresql://postgres:MDMD2121@localhost/python")
+db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
